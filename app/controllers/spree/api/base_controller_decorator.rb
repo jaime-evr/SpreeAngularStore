ba2013dba6f2b3995367a0bb91cfe7df5925031a
Jaime Victoria <jaime.victoria@crowdint.com>
2014-11-02 18:35:31 -0600

    rename variable naming and use default user params

--- app/controllers/spree/api/base_controller_decorator.rb
+++ app/controllers/spree/api/base_controller_decorator.rb
@@ -3,7 +3,7 @@ Spree::Api::BaseController.class_eval do
   skip_before_filter :authenticate_user, if: :create_user_action?
 
   def create_user_action?
-    req = request.path_parameters
-    req[:controller] === 'spree/api/users' && req[:action] == 'create'
+    location = request.path_parameters
+    location[:controller] == 'spree/api/users' && location[:action] == 'create'
   end
 end
--- app/controllers/spree/api/users_controller_decorator.rb
+++ app/controllers/spree/api/users_controller_decorator.rb
@@ -1,10 +1,11 @@
 Spree::Api::UsersController.class_eval do
+
   def create
     authorize! :create, Spree.user_class
     @user = Spree.user_class.new(user_params)
     if @user.save && @user.generate_spree_api_key!
       retrieve_spree_api_key
-      respond_with(@user, :status => 201, :default_template => :show)
+      respond_with(@user, status: 201, default_template: :show)
     else
       invalid_resource!(@user)
     end
